335c35925f6744c38a56897c893d8ecbdbdc0374
vinicius <vinicius>
2019-10-29 16:15:15 -0300

    Aula 5

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,22 +6,32 @@ class NegociacaoController{
         this._inputData = $('#data');
         this._inputQuantidade =  $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
+
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     adiciona(event){
         event.preventDefault();
-        
-        //let data = new Date(this._inputData.value.replace(/-/g, ','));
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._listaNegociacoes);
+        this._limpaFormulario();
+    }
 
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value
-        );
+    _criaNegociacao() {
+        return new Negociacao(
+              DateHelper.textoParaData(this._inputData.value),
+              this._inputQuantidade.value,
+              this._inputValor.value
+          );
+    }
 
-        console.log(negociacao);
-
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 
 }
